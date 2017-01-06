adbc5f6565085d23200ecf44222f277a70e833b6
gtteamamxx <grs4_98@o2.pl>
2017-01-06 12:55:57 +0100

    launcher

---
+++ b3dteam/FilePickerWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace b3dteam
+{
+    /// <summary>
+    /// Interaction logic for FilePickerWindow.xaml
+    /// </summary>
+    public partial class FilePickerWindow : Window
+    {
+        public delegate void pickerResultChanged(bool result);
+        public event pickerResultChanged OnPickerResultChanged;
+
+        public string Ball3DPath = string.Empty;
+
+        public FilePickerWindow()
+        {
+            InitializeComponent();
+
+            if (File.Exists(textbox_FilePath.Text))
+            {
+                Ball3DPath = textbox_FilePath.Text;
+                button_Accept.IsEnabled = true;
+            }
+        }
+
+        private void button_Accept_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button_Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button_FilePicker_Click(object sender, RoutedEventArgs e)
+        {
+            OnPickerResultChanged?.Invoke(AskUserForBall3DExeFile());
+        }
+
+        public bool AskUserForBall3DExeFile()
+        {
+            Microsoft.Win32.OpenFileDialog openPicker = new Microsoft.Win32.OpenFileDialog()
+            {
+                DefaultExt = ".exe",
+                Filter = "Ball 3D|Ball 3D.exe;"
+            };
+
+            bool? result = openPicker.ShowDialog();
+
+            if (result == true)
+            {
+                Ball3DPath = textbox_FilePath.Text = openPicker.FileName.ToString();
+                button_Accept.IsEnabled = true;
+                return true;
+            }
+            else
+            {
+                Ball3DPath = textbox_FilePath.Text = "You have to select a file!.";
+                button_Accept.IsEnabled = false;
+                return false;
+            }
+        }
+
+    }
+}
--- b3dteam/MainWindow.xaml.cs
+++ b3dteam/MainWindow.xaml.cs
@@ -20,9 +20,75 @@ namespace b3dteam
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string Ball3DPath = string.Empty;
+        public bool IsBall3DFileCorrect => Ball3DPath.Contains(".exe");
+
         public MainWindow()
         {
             InitializeComponent();
+
+            this.Loaded += (a, b) =>
+            {
+                var ball3dExePath = Properties.Settings.Default.Ball3DExePath;
+
+                if (ball3dExePath == "None")
+                {
+                    SelectBall3DFile();
+
+                    if (IsBall3DFileCorrect)
+                    {
+                        button_selectFile.Visibility = Visibility.Collapsed;
+                        text_Info.Content = "";
+                    }
+                }
+                else
+                {
+                    //AskUserForStatus();
+                }
+            };
+        }
+
+        public void SelectBall3DFile()
+        {
+            text_Info.Content = "Waiting for select 'Ball 3D.exe'...";
+
+            var filePickerWindow = new FilePickerWindow();
+
+            filePickerWindow.OnPickerResultChanged += (result) =>
+            {
+                if (result)
+                {
+                    Ball3DPath = filePickerWindow.Ball3DPath;
+                }
+            };
+
+            filePickerWindow.Closed += (s, e) =>
+            {
+                if (Ball3DPath == string.Empty)
+                {
+                    if (filePickerWindow.Ball3DPath == string.Empty || filePickerWindow.Ball3DPath.Contains("You have"))
+                    {
+                        text_Info.Content = "You have to select 'Ball 3D.exe'! first!";
+                    }
+                    else
+                    {
+                        Ball3DPath = filePickerWindow.Ball3DPath;
+                    }
+                }
+            };
+
+            filePickerWindow.ShowDialog();
+        }
+
+        private void button_selectFile_Click(object sender, RoutedEventArgs e)
+        {
+            SelectBall3DFile();
+
+            if (IsBall3DFileCorrect)
+            {
+                button_selectFile.Visibility = Visibility.Collapsed;
+                text_Info.Content = "";
+            }
         }
     }
 }
